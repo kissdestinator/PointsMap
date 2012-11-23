e6c5afe0c6d29ae250d65c310eb3b2628cdf0b58
Auzias <auzias.mael+_github@gmail.com>
2012-11-22 19:15:07 -0600

    Better with all the files x)

---
+++ src/class/Reader.cpp
@@ -0,0 +1,16 @@
+//=======================================================================
+// Basic C++: classe Map
+//      Specification of class Map
+//-----------------------------------------------------------------------
+// Auzias Maël - Constantina Ioannou
+// For the project: http://bit.ly/kiss-web
+//=======================================================================
+
+/*!
+ * \file Reader.cpp
+ *
+ * \brief Implementation of class Reader.
+ */
+
+#include "Reader.h"
+using namespace std;
---
+++ src/tests/test_Reader.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../class/Reader.h"
+using namespace std;
+
+/**
+ * Test for the Reader class.
+ * Create some Reader using the csv file name file.csv
+ */
+int main() {
+
+  return 0;
+}
