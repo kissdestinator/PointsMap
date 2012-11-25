331aba5ac53f5afefe1d92aba45e4c0cda4859b4
Lucas Grzegorczyk <lucasgrzegorczyk@gmail.com>
2012-11-26 00:14:25 +0100

    Made roads using specs from Google doc

--- src/tests/test_Road.cpp
+++ src/tests/test_Road.cpp
@@ -10,28 +10,46 @@ using namespace std;
  */
 int main() {
 
-Destination d[3];
-for (int i=0; i<3; i++){
-  d[i].set_id(i);
-}
-  d[0].set(0,0);
-  d[1].set(1,1);
-  d[2].set(1,0);
-  d[0].add_destination(1);        // Triangle of destinations
-  d[0].add_destination(2);
-  d[1].add_destination(0);
-  d[1].add_destination(2);
-  d[2].add_destination(0);
-  d[2].add_destination(1);
+  Destination d[7];                                            // Creating destinations to reflect CSV file
+  for (int i=0; i<7; i++){
+    d[i].set_id(i);
+  }
 
+  d[0].set(10,50);                                             // Coordinates multiplied by 10 because Mael included halfs
+  d[1].set(50,0);
+  d[2].set(75,10);
+  d[3].set(60,30);
+  d[4].set(40,50);
+  d[5].set(5,55);
+  d[6].set(35,30);
+  int arr0[] = {1,4,5};
+  int arr1[] = {0,2,3};
+  int arr2[] = {1,4};
+  int arr3[] = {0,3,5,6};
+  int arr4[] = {0,4};
+  int destArr[][2] = {{0,5},{5,0},{0,4},{4,0},{5,4},{4,5},{4,6},{6,4},{3,4},{4,3},{0,1},{1,0},{1,2},{2,1},{1,3},{3,1}};
 
-  void* raw_memory = operator new[]( 6 * sizeof( Road ) );    //creating 6 roads, back and forth
+  std::list<int> list0 (arr0, arr0 + sizeof(arr0) / sizeof(int));
+  std::list<int> list1 (arr1, arr1 + sizeof(arr1) / sizeof(int));
+  std::list<int> list2; 
+  list2.push_back(1);
+  std::list<int> list3 (arr2, arr2 + sizeof(arr2) / sizeof(int));
+  std::list<int> list4 (arr3, arr3 + sizeof(arr3) / sizeof(int));
+  std::list<int> list5 (arr4, arr4 + sizeof(arr4) / sizeof(int));
+  std::list<int> list6;
+  list6.push_back(4);
+  d[0].set_destinations(list0);                                 // Coordinates multiplied by 10 because Mael included halfs
+  d[1].set_destinations(list1);
+  d[2].set_destinations(list2);
+  d[3].set_destinations(list3);
+  d[4].set_destinations(list4);
+  d[5].set_destinations(list5);
+  d[6].set_destinations(list6);
+
+  void* raw_memory = operator new[]( 16 * sizeof( Road ) );    //creating 6 roads, back and forth
   Road* roads = static_cast<Road*>( raw_memory );
-  for( int i = 0; i < 6; ++i ) {
-    if (i<3)
-      new( &roads[i] )Road(d[i%3],d[(i+1)%3]);
-    if (i>2)
-      new( &roads[i] )Road(d[(i+1)%3],d[i%3]);
+  for (int i = 0; i < 16; ++i) {
+    new( &roads[i] )Road(d[destArr[i][0]],d[destArr[i][1]]);
     roads[i].set_id(i);
     cout << roads[i] << endl;
   }
